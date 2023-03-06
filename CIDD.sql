4d31bd6fb227619286c219f68630643afb0aa910
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2023-03-06 16:06:09 -0700

    b

--- CIDD.sql
+++ CIDD.sql
@@ -1,36 +1,68 @@
---Table for the patients
-CREATE TABLE patients(
-    patient_id INT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,   
-)
+CREATE TABLE Disease (
+  disease_id INT PRIMARY KEY,
+  strain_name VARCHAR(50),
+  rate_of_spread DECIMAL(4,2),
+  vaccine_type VARCHAR(50),
+  vaccine_efficacy DECIMAL(4,2)
+);
 
---Table for the doctors
-CREATE TABLE doctors(
-    doctor_id INT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,   
-)
+CREATE TABLE Affliction (
+  affliction_id INT PRIMARY KEY,
+  strain_name VARCHAR(50),
+  fatality_rate DECIMAL(4,2),
+  symptom_type VARCHAR(50),
+  spread_type VARCHAR(50),
+  hereditary BOOLEAN,
+  disease_id INT,
+  FOREIGN KEY (disease_id) REFERENCES Disease(disease_id)
+);
 
---Table for the appointments
-CREATE TABLE appointments(
-    appointment_id INT PRIMARY KEY,
-    patient_id INT NOT NULL,
-    doctor_id INT NOT NULL,
-    appointment_date DATE NOT NULL,
-    appointment_time TIME NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
-)
+CREATE TABLE Patient (
+  phone_number VARCHAR(20) PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  address VARCHAR(100),
+  hospital_id INT,
+  FOREIGN KEY (hospital_id) REFERENCES Hospital(hospital_id)
+);
 
---Table for the diagnoses
-CREATE TABLE diagnoses(
-    diagnosis_id INT PRIMARY KEY,
-    patient_id INT NOT NULL,
-    doctor_id INT NOT NULL,
-    diagnosis_date DATE NOT NULL,
-    diagnosis_time TIME NOT NULL,
-    diagnosis VARCHAR(100) NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
-)
+CREATE TABLE Hospital (
+  hospital_id INT PRIMARY KEY,
+  name VARCHAR(100),
+  address VARCHAR(100),
+  type VARCHAR(20)
+);
+
+CREATE TABLE Treatment (
+  treatment_id INT PRIMARY KEY,
+  method VARCHAR(50),
+  patient_phone VARCHAR(20),
+  FOREIGN KEY (patient_phone) REFERENCES Patient(phone_number)
+);
+
+CREATE TABLE Outbreak (
+  outbreak_id INT PRIMARY KEY,
+  location VARCHAR(100),
+  ongoing BOOLEAN,
+  num_confirmed_cases INT,
+  affliction_id INT,
+  FOREIGN KEY (affliction_id) REFERENCES Affliction(affliction_id)
+);
+
+CREATE TABLE Researcher (
+  ssn VARCHAR(20) PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
+);
+
+CREATE TABLE PHO (
+  ssn VARCHAR(20) PRIMARY KEY,
+  name VARCHAR(100),
+  address VARCHAR(100)
+);
+
+CREATE TABLE Doctor (
+  ssn VARCHAR(20) PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
+);
